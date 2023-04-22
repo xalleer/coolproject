de3c275bc00589b49a2c2071edbdcbe7c1453ab3
Sviatoslav Yaroshenko <quex.modest@gmail.com>
2023-04-22 15:02:14 +0300

    Fix bag

--- src/Frontend/js/script.js
+++ src/Frontend/js/script.js
@@ -1,7 +1,11 @@
-const newBlock = ('<div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 d-flex justify-content-center block-check"></div>');
+const newBlock = ('<div class="col-xl-2 col-lg-3 col-md-3 col-sm-12 d-flex justify-content-center block-check"></div>');
 const btnAdd = document.querySelector(".btn-add");
 const listTovar = document.querySelector(".block-check");
+
+
 
 $(btnAdd).click(function() {
  $(listTovar).parent().append(newBlock);
 });
+
+
